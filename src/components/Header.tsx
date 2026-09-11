176a430308b0fdc786b3cfc9b457eb915209fcbe
adeliaterco <adeliarosaterco@gmail.com>
2026-09-11 13:52:11 -0300

    hdhd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,7 @@ const Header = ({ showLogout = true }: HeaderProps) => {
             >
               <span className="text-2xl">💙</span>
               <span className="font-display font-bold text-lg">
-                <span className="text-primary">MRD</span> RECONQUISTA + SCRIPTS
+                <span className="text-primary">MRD</span> RECONQUISTA + SCRIPTS + DIAGNÓSTICO
               </span>
             </div>
           </div>
